da4ee0858ce8d2ea751abac1c46afb242bc537bb
Miguel Ceballos <ceballosmiguel777@gmail.com>
2017-04-24 10:44:10 -0700

    Redid the webstie

---
+++ Capstone.js
@@ -0,0 +1,18 @@
+function getPokemonAjax() {
+  $.ajax({
+    url: queryURL,
+    //dataType: 'jsonp',
+    success: function(data) {
+      displayPokemon(data)
+    }
+  })
+}
+
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
