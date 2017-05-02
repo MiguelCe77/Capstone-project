1752dc6749ff149c76d9db902f76ac1aa80db14b
Miguel Ceballos <ceballosmiguel777@gmail.com>
2017-05-02 08:48:54 -0700

    Added  characters tab

--- Capstone.js
+++ Capstone.js
@@ -1,15 +1,44 @@
-var queryURL = "http://smashlounge.com/api/techs/all"
+var queryURL = "http://smashlounge.com/api/chars/all"
+var numberOfCharacters = 26
+
+function randomNumber(min,max)
+{
+ return Math.floor(Math.random()*(max-min+1)+min)
+}
 
 
-function getPokemonAjax() {
+function getCharacterAjax() {
   $.ajax({
     url: queryURL,
     //dataType: 'jsonp',
     success: function(data) {
-      displayTech(data)
+      displayCharacter(data)
     }
   })
 }
+
+function randomCharacterButtonClick() {
+  var rand = randomNumber(1, numberOfCharacters)
+
+  // when the user clicks the button, you will do two things:
+  // 1) Assign a string to queryURL
+  // 2) call the getPokemonAjax() function
+   getCharacterAjax()
+}
+
+function displayCharacter(data) {
+
+  var name = data['name']
+  var guide = data['guide']
+
+  var textString = "<p>Name: " + name + "</br>"
+  textString += "Info: " + guide + "</br>"
+
+  document.getElementById("datalist").innerHTML += textString
+
+   getCharacterAjax()
+}
+
 
 
 
